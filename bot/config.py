0a80cbb1b10c5402fc6d7fde496ccb30b7c0c1da
luciferr169 <62810628+luciferr169@users.noreply.github.com>
2023-09-24 00:39:59 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,10 +2,10 @@ import os
 from os import getenv
 
 class Config:
-    TELEGRAM_TOKEN = getenv("TELEGRAM_TOKEN", None)
+    TELEGRAM_TOKEN = getenv("5889234320:AAHIHuwTbejv-kysiT6DU4iAX3qyRZbQhbs", None)
     PYRO_SESSION = getenv("PYRO_SESSION", None)
     TELEGRAM_APP_HASH= getenv('TELEGRAM_APP_HASH')
-    TELEGRAM_APP_ID=int(getenv('TELEGRAM_APP_ID'))
+    TELEGRAM_APP_ID=int(getenv('Grouphellpb_bot'))
         
     if not TELEGRAM_APP_HASH:
         raise ValueError("TELEGRAM_APP_HASH not set")
